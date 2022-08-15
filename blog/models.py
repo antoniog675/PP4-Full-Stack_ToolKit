517a5f8a761ec267c677130908f2d1380f500d3b
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-15 18:48:59 +0000

    added migrations

--- blog/models.py
+++ blog/models.py
@@ -2,25 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "draft"), (1, "published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    spirit = models.ForeignKey(Categories, related_name='type_of_spirit')
-    slug = models.SlugField(max_length=200, unqiue=True)
+    # spirit = models.ForeignKey(Categories)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+
 
     class Meta:
         ordering = ["-created_on"]
 
-    def __str__(self):
+    def _str_(self):
         return self.title
 
     def number_of_likes(self):
