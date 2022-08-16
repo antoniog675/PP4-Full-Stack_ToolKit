ecef5ac97093933d041a3285c7dde5f6a2dac9a2
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-16 11:54:31 +0000

    admin able to create posts and approve comments on post

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,22 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'ingredients']
     summernote_fields = ('ingredients', 'instructions')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approved_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
 
