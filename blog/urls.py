5738c55907c86f2e6e0dc12ff328d0323537a873
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-19 15:08:29 +0000

    wired drinks detail to post on post list

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 urlpatterns = [
     path('', views.home, name='home'),
     path('explore/', views.PostList.as_view(), name='explore'),
+    path('<slug:slug>/', views.DrinksDetail.as_view(), name='drinks_detail')
 ]
