a774bedb3d58a771f890eb0c36424ebbf78bb762
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-19 15:53:01 +0000

    added drinks detail, everything is being viewed as expected

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class DrinksDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.user.id).exist():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
         
         return render(
