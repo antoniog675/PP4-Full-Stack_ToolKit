aafefedfe55abe8d2d99357b66f930d809b3b0bb
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-22 13:22:10 +0000

    Added comment section form but it isn't loading

--- cocktailz/settings.py
+++ cocktailz/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'blog',
     'django_summernote'
 ]
@@ -58,6 +59,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
