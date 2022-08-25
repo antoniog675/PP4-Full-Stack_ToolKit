32da6aa85709e1703936364e51a03da3cf4fbce4
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-25 11:25:56 +0000

    users able to like posts, and it will be store in the favourites section where the can also view it

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('explore/', views.PostList.as_view(), name='explore'),
     path('add_drinks/', views.add_drinks, name='add_drinks'),
+    path('drinks_favourites/', views.PostFavourites.as_view(), name='favourites'),
+    path('drinks_favourites/<slug:slug>/', views.PostFavourites.as_view(), name='post_favourites'),
     path('<slug:slug>/', views.DrinksDetail.as_view(), name='drinks_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
+from django.views.generic import UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm, PostDrinkForm
-from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -95,6 +95,67 @@ class PostLike(View):
         return HttpResponseRedirect(reverse('drinks_detail', args=[slug]))
 
 
-# class PostDrafts
+class PostFavourites(generic.ListView):
+    model = Post
+    template_name = 'drinks_favourites.html'
+    paginate_by = 6
+    context_object_name = 'post_favourites'
 
-# class PostFavourites
+    def get_queryset(self):
+        user = self.request.user
+        queryset = user.blogpost_like.all()
+        return queryset
+
+
+class FavouritesDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "drinks_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        
+        else:
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            "drinks_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
