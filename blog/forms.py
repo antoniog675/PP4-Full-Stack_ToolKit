0f3f57a501108fa21ef56f452ee853358c0a4446
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-08-26 16:01:22 +0000

    added docstrings to comply with pylint standard

--- blog/forms.py
+++ blog/forms.py
@@ -3,18 +3,22 @@ These are the forms used for user comments, and if user
 wishes to upload a drink the PostDrinkForm is used to
 retrieve information to build the post
 """
-from .models import Comment, Post
 from django import forms
+from .models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
+    """Comment Form  so users can comment on posts"""
     class Meta:
+        """Meta for CommentForm"""
         model = Comment
         fields = ('body',)
 
 
 class PostDrinkForm(forms.ModelForm):
+    """PostDrinkForm for user to post drink"""
     class Meta:
+        """Meta for PostDrinkForm"""
         model = Post
         fields = (
             'title', 'spirit', 'ingredients', 'instructions',
